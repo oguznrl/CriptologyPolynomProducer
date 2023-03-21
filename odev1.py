f76ef8e97de496f406df7241a07ffa022127226a
Oğuz Narlı <47397376+oguznrl@users.noreply.github.com>
2023-03-21 22:42:28 +0300

    Add files via upload

--- odev1.py
+++ odev1.py
@@ -8,11 +8,17 @@ for i in polynom:
         pass
     else:
         mod.append(i)
+def unique(list):
+    unique_list = []
+    for x in list:
+        if x not in unique_list:
+            unique_list.append(x)
+    return unique_list
 
 polynom_subvalues=[]
 def expand_result(value):
-    count_of_border=i-border_limit
-    else_multipiler=i-count_of_border*border_limit
+    count_of_border=int(value/border_limit)
+    else_multipiler=value-(count_of_border*border_limit)
     is_first=False
     expanded_mod=[]
     temp_mod=list()
@@ -27,17 +33,18 @@ def expand_result(value):
                 for j in range(len(mod)):
                     for k in range(len(mod)): 
                         expanded_mod.append(mod[j]+mod[k])
+                for j in range(len(expanded_mod)):
+                    temp_mod.append(expanded_mod[j]+else_multipiler)
             else:
-                count_of_border-=1
-                for j in range(len(mod)):
-                    for k in range(len(mod)): 
-                        temp_mod.append(mod[j]+mod[k])
-                expanded_mod.clear()
-                for j in temp_mod:
-                    expanded_mod.append(j)
                 temp_mod.clear()
-            for j in range(len(expanded_mod)):
-                temp_mod.append(expanded_mod[j]+else_multipiler)
+                count_of_border-=1
+                for j in range(len(expanded_mod)):
+                    for k in range(len(mod)): 
+                        temp_mod.append(expanded_mod[j]+mod[k])
+            expanded_mod.clear()
+            for j in range(len(temp_mod)):
+                expanded_mod.append(temp_mod[j])
+            
 
     expanded_mod.clear()
     for j in temp_mod:
@@ -53,25 +60,31 @@ for i in range(pow(2,border_limit)):
             polynom_subvalues.append(mod)
         else:
             expanded_res=[i]
-            while(max(expanded_res)>border_limit):
+            max_res=max(expanded_res)
+            while(max_res>=border_limit):
                 for j in range(len(expanded_res)):
                     if(expanded_res[j]>=4):
                         res=expand_result(expanded_res[j])
                         expanded_res[j]=res[0]
                         for k in range(1,len(res)):
-                            expanded_res.append(k)
+                            expanded_res.append(res[k])
+                odd_number=list()
                 for j in range(len(expanded_res)):
-                    temp_val=None
-                    if(expanded_res.count(expanded_res[j])>1):
-                        temp_val=expanded_res[j]
-                        expanded_res.remove(expanded_res[j])
-                        expanded_res.append(temp_val)
+                    if(expanded_res.count(expanded_res[j])%2==0):
+                        odd_number.append(expanded_res[j])
+                expanded_res=unique(expanded_res)
+                odd_number=unique(odd_number)
+                for j in odd_number:
+                    if(j in odd_number):
+                        expanded_res.remove(j)
+                max_res=max(expanded_res)
             polynom_subvalues.append(expanded_res)
 
+equal_value=[]
+for i in range(len(polynom_subvalues)-1):
+    res=0
+    for j in polynom_subvalues[i]:
+        res+=pow(2,j)
+    equal_value.append(res)
 
-            
-            
-                   
-                
-                        
-
+print(equal_value)
